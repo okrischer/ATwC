e49e7eebe78805a2aa66cd98126c3150dcc8cc92
Oliver Krischer <oliver.krischer@gmail.com>
2021-08-31 17:18:40 +0200

    optimized haskell solution for problem 1

--- 01-HashTables/UniqueSnowflakes.hs
+++ 01-HashTables/UniqueSnowflakes.hs
@@ -1,13 +1,15 @@
 import qualified Data.ByteString.Char8 as B
-import qualified Data.Map as M
+import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 import Data.Char (isDigit)
-import Data.List (unfoldr, splitAt)
+import Data.List (unfoldr)
+import Data.Maybe (isNothing, fromMaybe)
 
 main :: IO ()
 main = do
   _ <- getLine
-  snowflakes <- getSnowflakes
+  sf <- getSnowflakes
+  let snowflakes = convert sf
   if foundIdentical snowflakes
      then putStrLn "Twin snowflakes found."
      else putStrLn "No two snowflakes are alike."
@@ -18,8 +20,8 @@ getSnowflakes = B.getLine >>= f
                           else getSnowflakes >>= g
                              where g xs = return (x:xs)
 
-foundIdentical :: [B.ByteString] -> Bool
-foundIdentical = findDuplicate . getMap . convert
+foundIdentical :: [[Integer]] -> Bool
+foundIdentical sf = searchMap sf M.empty
 
 convert :: [B.ByteString] -> [[Integer]]
 convert = map (unfoldr readInteger)
@@ -27,51 +29,37 @@ convert = map (unfoldr readInteger)
 readInteger :: B.ByteString -> Maybe (Integer, B.ByteString)
 readInteger = B.readInteger . B.dropWhile (not . isDigit)
 
-getMap :: [[Integer]] -> M.Map Integer [Integer]
-getMap sf = createMap (sf, M.empty)
-
-createMap :: ([[Integer]], M.Map Integer [Integer]) -> M.Map Integer [Integer]
-createMap ([], acc) = acc
-createMap (xs:xss, acc) = createMap (xss, M.insertWith (++) hash xs acc)
-  where hash = getHash xs 1000
-
-getHash :: [Integer] -> Integer -> Integer
-getHash sf n = sum sf `rem` n
-
-findDuplicate :: M.Map Integer [Integer] -> Bool
-findDuplicate sf =
-  let potDups = M.filterWithKey (\k a -> length a > 6) sf
-  in M.size potDups /= 0 && checkIdent (M.elems potDups)
-
-checkIdent :: [[Integer]] -> Bool
-checkIdent [] = False
-checkIdent (xs:xss) = identical dups || checkIdent xss
-  where dups = split xs
+searchMap :: [[Integer]] ->  M.Map Integer [Integer] -> Bool
+searchMap [] _ = False
+searchMap (xs:xss) acc
+  | isNothing result = searchMap xss (M.insert hash xs acc)
+  | contains values xs = True
+  | otherwise = searchMap xss (M.insertWith (++) hash xs acc)
+  where hash = sum xs `mod` 100000
+        result = M.lookup hash acc
+        values = fromMaybe [] result
   
+contains :: [Integer] -> [Integer] -> Bool
+contains values = checkIdent (split values)
+
 split :: [Integer] -> [[Integer]]
 split [] = []
 split xs = fst pair : split (snd pair)
-  where pair = splitAt 6 xs  
+  where pair = splitAt 6 xs
 
-identical :: [[Integer]] -> Bool
-identical [] = False
-identical (xs:xss) = equals xs xss || identical xss
-
-equals :: [Integer] -> [[Integer]] -> Bool
-equals _ [] = False
-equals xs (ys:yss) =
-  identPair xs ys || equals xs yss
+checkIdent :: [[Integer]] -> [Integer] -> Bool
+checkIdent [] _ = False
+checkIdent (xs:xss) sf = identPair xs sf || checkIdent xss sf
 
 identPair :: [Integer] -> [Integer] -> Bool
-identPair xs ys =
-  (S.difference set1 set2 == S.empty && S.difference set2 set1 == S.empty) &&
-    isEqual xs ys
-  where set1 = S.fromList xs
-        set2 = S.fromList ys
+identPair xs ys = S.fromList xs == S.fromList ys && areEqual xs ys
 
-isEqual :: [Integer] -> [Integer] -> Bool 
-isEqual [] _ = True
-isEqual _ [] = False
-isEqual s1@(x:xs) s2@(y:ys) =
-  if x == y then isEqual xs ys
-            else isEqual s1 (ys ++ [y])
+areEqual :: [Integer] -> [Integer] -> Bool 
+areEqual xs ys
+  | x /= y =
+    let shiftL = dropWhile (/= x) ys ++ takeWhile (/= x) ys
+        shiftR = reverse (tail (dropWhile (/= x) ys) ++ takeWhile (/= x) ys ++ [x])
+    in xs == shiftL || xs == shiftR
+  | otherwise = xs == ys
+  where x = head xs
+        y = head ys
